776bace84416c5d3353799b0a0130c9f7c0d1775
AlejandroCB-23 <Alejandro.Canas1@alu.uclm.es>
2023-12-04 10:59:01 +0100

    ACtualización

--- icedrive_authentication/authentication.py
+++ icedrive_authentication/authentication.py
@@ -27,6 +27,10 @@ class User(IceDrive.User):
     def refresh(self, current: Ice.Current = None) -> None:
         """Renew the authentication for 1 more period of time."""
         #Comprobamos que el usuario siga en el txt
+
+        if not self.persistencia.verificar_usuario_en_archivo(self.username):
+            raise IceDrive.UserNotExist
+        
         if not self.persistencia.verificar_usuario_en_archivo(self.username, self.password):
             raise IceDrive.Unauthorized
         
@@ -43,7 +47,7 @@ class Authentication(IceDrive.Authentication):
     def login(
         self, username: str, password: str, current: Ice.Current = None
     ) -> IceDrive.UserPrx:
-        """Authenticate an user by username and password and return its User.Comprobando que esten en el txt tambien"""
+        """Authenticate an user by username and password and return its User. Comprobando que esten en el txt tambien"""
             
         # Verificar si el usuario existe en el archivo y validar la contraseña
         if not self.persistencia.verificar_usuario_en_archivo(username, password):
@@ -84,8 +88,10 @@ class Authentication(IceDrive.Authentication):
         if self.persistencia.verificar_usuario_en_archivo(username):
                 raise IceDrive.UserAlreadyExists
 
+        #Creamos el usuario con el nombre y la contraseña
         usuario = User(username, password)
-        usuario_proxy = IceDrive.UserPrx.uncheckedCast( current.adapter.addWithUUID(usuario).ice_timeout(120).ice_twoway().ice_secure(False))
+        #Creamos el proxy del usuario y lo añadiomos al adapter
+        usuario_proxy = IceDrive.UserPrx.uncheckedCast(current.adapter.addWithUUID(usuario).ice_timeout(120).ice_twoway().ice_secure(False))
 
         # Guardar el UUID del usuario en el archivo
         self.persistencia.guardar_usuario_en_archivo(username, password, usuario_proxy.ice_getIdentity().name)
@@ -98,11 +104,12 @@ class Authentication(IceDrive.Authentication):
     ) -> None:
         """Remove the user "username" if the "password" is correct."""
 
+        #Verificamos que la contraseña y el nombre son correctos
         if not self.persistencia.verificar_usuario_en_archivo(username, password):
             raise IceDrive.Unauthorized
 
         try:
-            # Verificar si el usuario existe en el archivo y obtener el UUID
+            # Obtenemos los UUID
             uuid = self.persistencia.obtener_uuid_de_archivo(username, password)
 
             if uuid is None:
